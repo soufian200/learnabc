ba27c44391779b010ddc9a2755b89b8940336cc9
soufian200 <soufianxlm@gmail.com>
2022-05-20 09:29:50 +0100

    rename app name

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final String bundleId = 'freegames';
+  final String bundleId = 'learnabc';
   @override
   void initState() {
     // TODO: implement initState
